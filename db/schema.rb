d351fe582c3bdc96c759a7184a747d8451a7f0d8
haruka0000 <1124226@st.anan-nct.ac.jp>
2015-03-14 22:54:50 +0900

    data pass system complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308024052) do
+ActiveRecord::Schema.define(version: 20150314135022) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "user_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "receipts", force: true do |t|
     t.integer  "owner_id"
